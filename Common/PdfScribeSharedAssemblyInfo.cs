2b9daaa0d5ef17da592bed28a457d73f3816a34e
Sarabpreet Bedi <bedisarabpreet@gmail.com>
2023-10-06 23:59:31 +0530

    first commit

---
+++ Common/PdfScribeSharedAssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Black Telescope Workshop")]
+[assembly: AssemblyProduct("PDF Scribe")]
+[assembly: AssemblyCopyright("Copyright © S T Chan 2013-2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.3.2.*")]
